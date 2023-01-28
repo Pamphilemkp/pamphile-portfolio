43e557d5855404f82f1ed188b93e5eb396d1296f
pamphilemkp <pamphilemkp@gmail.com>
2023-01-28 13:34:42 +0200

    add app project basic desktop style

---
+++ src/components/Projects/PopUp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PopUp() {
+  return (
+    <div>PopUp</div>
+  );
+}
+
+export default PopUp;
--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,16 +1,56 @@
-/* eslint-disable array-callback-return */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/jsx-props-no-spreading */
+import Slider from 'react-slick';
+import ProjectStore from './ProjectStore';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import PopUp from './PopUp';
 
-function Project(props) {
-  const { project } = props;
+function Project() {
+  const sliderSettings = {
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    centerMode: false,
+    draggable: true,
+    speed: 700,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+
+  };
+
   return (
-    project.map((project) => {
-      <div className="image-project">
-        <p>{project.title}</p>
-        <img src={project.photo} alt={project.title} />
-        <p>{project.description}</p>
-        {/* {console.log(project.title)} */}
-      </div>;
-    }));
+    <div className="project-contents">
+      <Slider {...sliderSettings}>
+        {ProjectStore.map(({ id, title, photo }) => (
+          <div key={id} onClick={<PopUp />}>
+            <img src={photo} alt={title} />
+            <p>{title}</p>
+          </div>
+        ))}
+        {/* <ul>{dots}</ul> */}
+      </Slider>
+    </div>
+  );
 }
-
 export default Project;
