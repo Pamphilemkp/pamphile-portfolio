d56df791587b9b3ddbde93470fb97d266ca8eb2a
pamphilemkp <pamphilemkp@gmail.com>
2023-02-07 20:21:29 +0200

    update SPA first portfolio version

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home/Home';
+import SomeProjects from '../components/Projects/SomeProjects';
+import AboutMe from '../components/About/AboutMe';
+import Contact from '../components/Contact/Contact';
 
 function HomePage() {
   return (
     <div className="home-container">
       <NavBar />
       <Home />
+      <SomeProjects />
+      <AboutMe />
+      <Contact />
     </div>
   );
 }
