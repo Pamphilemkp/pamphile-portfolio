97c115f1266b68785d33fc41300145b88c86a8c1
pamphilemkp <pamphilemkp@gmail.com>
2023-02-14 14:31:51 +0200

    new app bacground and animation effect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import ParticleBackground from 'react-particle-backgrounds';
 import HomePage from './pages/HomePage';
 import About from './pages/About';
 import Skills from './pages/Skills';
 import Projects from './pages/Projects';
 import Contacts from './pages/Contacts';
-// import AppBackground from './components/AppBackground';
 import PageNotFound from './pages/PageNotFound';
 
 function App() {
+  const settings = {
+    particle: {
+      particleCount: 75,
+      color: '#fff',
+      minSize: 3,
+      maxSize: 10,
+    },
+    velocity: {
+      minSpeed: 0.5,
+      maxSpeed: 1,
+    },
+    opacity: {
+      minOpacity: 0,
+      maxOpacity: 0.6,
+      opacityTransitionTime: 10000,
+    },
+  };
+
   return (
     <div className="App">
+      <ParticleBackground settings={settings} className="app-global-bg" />
       <Routes>
         <Route path="/" element={<HomePage />}> </Route>
         <Route path="/About" element={<About />} />
@@ -19,7 +39,6 @@ function App() {
         <Route path="/Contacts" element={<Contacts />} />
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
-      {/* <AppBackground /> */}
     </div>
   );
 }
